26b832bf7dbb957958669b8180a37714966c7321
Lazar Dejanovic <lazzar99@gmail.com>
2022-12-12 01:58:28 +0100

    Initial commit

---
+++ src/test/java/com/raf/nwpdomaci3/NwpDomaci3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raf.nwpdomaci3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwpDomaci3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
