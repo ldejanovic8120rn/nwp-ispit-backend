0cbb93011116b7cff911f5f15c74753f9d8c3f6a
Lazar Dejanovic <lazzar99@gmail.com>
2023-02-17 19:35:02 +0100

    bootstrapping data

--- src/main/java/com/raf/nwpispit/bootstrap/BootstrapData.java
+++ src/main/java/com/raf/nwpispit/bootstrap/BootstrapData.java
@@ -1,8 +1,10 @@
 package com.raf.nwpispit.bootstrap;
 
+import com.raf.nwpispit.domain.entities.machine.Machine;
 import com.raf.nwpispit.domain.entities.user.Role;
 import com.raf.nwpispit.domain.entities.user.RoleType;
 import com.raf.nwpispit.domain.entities.user.User;
+import com.raf.nwpispit.repository.MachineRepository;
 import com.raf.nwpispit.repository.RoleRepository;
 import com.raf.nwpispit.repository.UserRepository;
 import com.sun.tools.javac.util.List;
@@ -10,21 +12,29 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+import java.util.Date;
+
 @Component
 public class BootstrapData implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final MachineRepository machineRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public BootstrapData(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
+    public BootstrapData(UserRepository userRepository, MachineRepository machineRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.machineRepository = machineRepository;
         this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... args) {
+        User userAdmin = new User();
+
+        //ROLES
         Role roleCreate = new Role();
         roleCreate.setRole(RoleType.CAN_CREATE);
 
@@ -37,13 +47,48 @@ public class BootstrapData implements CommandLineRunner {
         Role roleDelete = new Role();
         roleDelete.setRole(RoleType.CAN_DELETE);
 
+        Role roleCanSearchMachine = new Role();
+        roleCanSearchMachine.setRole(RoleType.CAN_SEARCH_MACHINE);
 
-        User userAdmin = new User();
+        Role roleCanCreateMachine = new Role();
+        roleCanSearchMachine.setRole(RoleType.CAN_CREATE_MACHINE);
+
+        Role roleCanDestroyMachine = new Role();
+        roleCanSearchMachine.setRole(RoleType.CAN_DESTROY_MACHINE);
+
+        Role roleCanStartMachine = new Role();
+        roleCanSearchMachine.setRole(RoleType.CAN_START_MACHINE);
+
+        Role roleCanStopMachine = new Role();
+        roleCanSearchMachine.setRole(RoleType.CAN_STOP_MACHINE);
+
+        Role roleCanRestartMachine = new Role();
+        roleCanSearchMachine.setRole(RoleType.CAN_RESTART_MACHINE);
+
+
+        //MACHINES
+        Machine machine1 = new Machine();
+        machine1.setName("machine1");
+        machine1.setCreatedBy(userAdmin);
+        machine1.setCreatedDate(Date.from(Instant.now()));
+
+        Machine machine2 = new Machine();
+        machine2.setName("machine2");
+        machine2.setCreatedBy(userAdmin);
+        machine2.setCreatedDate(Date.from(Instant.now()));
+
+
+        //SETTING TO USER
         userAdmin.setFirstName("Admin");
         userAdmin.setLastName("Admin");
         userAdmin.setEmail("admin@admin.com");
         userAdmin.setPassword(passwordEncoder.encode("admin"));
-        userAdmin.setRoles(List.of(roleCreate, roleRead, roleUpdate, roleDelete));
+        userAdmin.setRoles(List.of(roleCreate, roleRead, roleUpdate, roleDelete, roleCanSearchMachine, roleCanCreateMachine,
+                roleCanDestroyMachine, roleCanStartMachine, roleCanStopMachine, roleCanRestartMachine
+        ));
+        userAdmin.setMachines(List.of(machine1, machine2));
+
+
 
         //posto je u useru @Cascade(CascadeType.PERSIST) -> ne sme da se radi!
 //        roleRepository.saveAll(List.of(roleCreate, roleRead, roleUpdate, roleDelete));
