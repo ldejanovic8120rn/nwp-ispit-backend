abd5b42531f704d36b2da58e21cb1f46199a2f54
Lazar Dejanovic <lazzar99@gmail.com>
2023-02-17 03:59:58 +0100

    finished searching machines by params

--- src/main/java/com/raf/nwpdomaci3/repository/MachineRepository.java
+++ src/main/java/com/raf/nwpdomaci3/repository/MachineRepository.java
@@ -1,10 +1,23 @@
 package com.raf.nwpdomaci3.repository;
 
 import com.raf.nwpdomaci3.domain.entities.machine.Machine;
+import com.raf.nwpdomaci3.domain.entities.machine.MachineStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface MachineRepository extends JpaRepository<Machine, Long> {
+
+    @Query("select m from Machine m where m.active = true " +
+            "and (m.createdBy.email = :email) " +
+            "and (:machineName is null or m.name like %:machineName%) " +
+            "and ((:statuses is null) or m.status in (:statuses)) " +
+            "and (cast(:dateFrom as date) is null or cast(:dateTo as date) is null or (m.createdAt between :dateFrom and :dateTo))"
+    )
+    List<Machine> findAllMachinesByParams(String email, String machineName, List<MachineStatus> statuses, Date dateFrom, Date dateTo);
 
 }
