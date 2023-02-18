6f15a8578d565082b6364396d20158847a6371dc
Lazar Dejanovic <lazzar99@gmail.com>
2023-02-18 05:12:31 +0100

    bug fixed

--- src/main/java/com/raf/nwpispit/repository/MachineRepository.java
+++ src/main/java/com/raf/nwpispit/repository/MachineRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -20,11 +21,11 @@ public interface MachineRepository extends JpaRepository<Machine, Long>{
 //    )
 @Query("select m from Machine m where m.createdBy.email = :email " +
         "and (:machineName is null or m.name like %:machineName%) " +
-        "and ((:statusList) is null  or m.status in :statusList) " +
+        "and ((:statusList) is null or m.status in (:statusList)) " +
         "and (cast(:dateFrom as date) is null or m.createdDate >= :dateFrom) " +
         "and (cast(:dateTo as date) is null or m.createdDate <= :dateTo) " +
         "and m.active = true ")
-    List<Machine> findAllMachinesByParams(String email, String machineName, List<MachineStatus> statusList, Date dateFrom, Date dateTo);
+    List<Machine> findAllMachinesByParams(String email, String machineName, Collection<MachineStatus> statusList, Date dateFrom, Date dateTo);
 
 //    List<Machine> findAllByActiveAndCreatedBy_EmailAndNameContainingIgnoreCaseAndStatusInAndCreatedDateBetween(
 //            boolean active, String email, String name, List<MachineStatus> statuses, Date dateFrom, Date dateTo
